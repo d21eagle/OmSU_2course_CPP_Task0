2097855a2ec80af4b5b0ecaa0a2849c2b940b9e6
Vlad Ilyashenko <vlad180317@gmail.com>
2022-05-06 14:32:46 +0300

    Task0_C++ last patch

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Box.h"
+#include "Container.h"
+using namespace Boxes;
+using namespace Containers;
+
+/*
+int main() {
+
+    std::cout << "Enter box parameters: " << endl;
+    Box box1(0, 0, 0, 0, 0);
+    std::cin >> box1;
+    std::cout << box1;
+    std::cout << endl << endl;
+
+    vector<Box> box_arr;
+    Box box10 = Box(0, 0, 0, 0.0, 0);
+    Box box20 = Box(0, 0, 0, 0.0, 0);
+    box_arr.push_back(box10);
+    box_arr.push_back(box20);
+
+    Container cont1 = Container(box_arr, 100, 200, 300, 60.0);
+    std::cout << "Enter containers parameters: " << endl;
+    std::cin >> cont1;
+    std::cout << cont1;
+}
+ */
